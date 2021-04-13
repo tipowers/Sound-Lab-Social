9c3a11e75c22ed8ef754a5b96bf22ee87cc13c82
tipowers <tpowers88@outlook.com>
2021-04-12 20:04:13 -0400

    setup DB connection string and successfully tested by creating test user account

---
+++ SoundLabSocial.Data/Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundLabSocial.Data
+{
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; }
+
+        [Required]
+        //Foreign Key here ....
+        //Also GUID ??? 
+        public int UserId { get; set; }
+
+        [Required]
+        public string SongName { get; set; }
+
+        [Required]
+        public string SongArtist { get; set; }
+
+        [Required]
+        public string SongAlbum { get; set; }
+
+        public DateTime ReleaseYear { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUTC { get; set; }
+
+        public DateTimeOffset? ModifiedUTC { get; set; }
+    }
+
+}
